a6c77cc0c3188589c9efb2d330f11f82d840da12
papadum_is_nice <jimmy_hsu@singnet.com.sg>
2019-10-01 12:46:25 +0800

    Fixed Scroll to Top

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,4 @@
-<script>
+
       //Get the button
       var mybutton = document.getElementById("myBtn");
       
@@ -18,4 +18,4 @@
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
       }
-      </script>
+    
